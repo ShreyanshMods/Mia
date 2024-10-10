5aa64531493ddd7c843b804df48374b2b78f474b
Shreyansh Mods <techsshreyansh@gmail.com>
2024-10-10 21:14:42 +0530

    Update welcome.py
    Signed-off-by: Shreyansh Mods <techsshreyansh@gmail.com>

--- FallenRobot/modules/welcome.py
+++ FallenRobot/modules/welcome.py
@@ -273,7 +273,7 @@ def new_member(update: Update, context: CallbackContext):
                     parse_mode=ParseMode.HTML,
                 )
                 update.effective_message.reply_text(
-                    "Watashi ga kita !", reply_to_message_id=reply
+                    "Welcome to the group! 😊!", reply_to_message_id=reply
                 )
                 continue
 
